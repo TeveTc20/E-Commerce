ee77f058ac885c103865e8a3fcc7b19d2b3d1a79
Liad Mizrahi <131164262+liadmiz1510@users.noreply.github.com>
2023-06-21 12:57:02 +0300

    sproduct page,home page

---
+++ views/sproduct/sproduct.js
@@ -0,0 +1,23 @@
+$.ajax({
+    url: 'product.json',
+    method: 'GET',
+    success: function (data) {
+        // Update product information
+        $('#productTitle').text(data.title);
+        $('#productPrice').text('$' + data.price);
+        $('#productDescription').text(data.description);
+
+        // Update carousel images
+        var carouselItems = '';
+        for (var i = 0; i < data.images.length; i++) {
+            var activeClass = (i === 0) ? 'active' : '';
+            carouselItems += '<div class="carousel-item ' + activeClass + '">';
+            carouselItems += '<img src="' + data.images[i] + '" class="img-fluid" alt="Product Photo ' + (i + 1) + '">';
+            carouselItems += '</div>';
+        }
+        $('#productCarousel .carousel-inner').html(carouselItems);
+    },
+    error: function () {
+        console.log('Error occurred while fetching data.');
+    }
+});
